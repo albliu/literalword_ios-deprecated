577104352b86d52cfbd2443d1f690e463542fce2
albliu <albliusz.2@gmail.com>
2012-04-10 21:53:14 -0700

    test search cell

--- Classes/ViewControllers/SearchViewController.h
+++ Classes/ViewControllers/SearchViewController.h
@@ -1,6 +1,12 @@
 #import "VersesViewController.h"
 
-@interface SearchViewController : VersesViewController <UISearchBarDelegate>
+#define CELLWEBVIEW 300
+#define CELLLABELVIEW 301
+
+#define SEARCH_RESULTS_FONT_SCALE 100
+#define VERSE_LABEL_HEIGHT 8 
+#define VERSE_LABEL_FONT_SIZE 10
+@interface SearchViewController : VersesViewController <UIWebViewDelegate, UISearchBarDelegate>
 {
     NSArray *searchData;
 }
--- Classes/ViewControllers/SearchViewController.m
+++ Classes/ViewControllers/SearchViewController.m
@@ -4,6 +4,21 @@
 
 @implementation SearchViewController
 
+#pragma mark - webview functions
+- (NSString *) searchHTMLString : (NSString *) verse {
+
+	NSString * ret =  [NSString stringWithFormat:@"<!DOCTYPE html><head>\
+<link href=\"body.css\" rel=\"stylesheet\" type=\"text/css\" />\
+<link href=\"literaray.css\" rel=\"stylesheet\" type=\"text/css\" />\
+<style type=\"text/css\">body {-webkit-text-size-adjust:%d%%;}</style>\
+</head><body>", SEARCH_RESULTS_FONT_SCALE];
+
+	ret = [ret stringByAppendingString:verse];
+	ret = [ret stringByAppendingString:[NSString stringWithUTF8String:"</body>"]];
+	return ret;	
+}
+
+
 
 #pragma mark - View lifecycle
 
@@ -40,6 +55,47 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{    
+    static NSString* CellIdentifier = @"WebCell";
+    UIWebView * webView = nil;
+    UILabel* verseLabel = nil;
+
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier: CellIdentifier];
+    if ( cell == nil ) 
+    {
+        cell = [[[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault 
+                                       reuseIdentifier: CellIdentifier] autorelease];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
+	webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, VERSE_LABEL_HEIGHT, self.view.bounds.size.width, 44 - VERSE_LABEL_HEIGHT)];
+	webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+	[webView setDelegate:self];
+	webView.tag = CELLWEBVIEW;
+        [cell.contentView addSubview: webView];
+	[webView release];
+
+        verseLabel = [[[UILabel alloc] initWithFrame: CGRectMake( 0, 0, self.view.bounds.size.width, VERSE_LABEL_HEIGHT )] autorelease];
+        verseLabel.tag = CELLLABELVIEW; 
+        verseLabel.font = [UIFont boldSystemFontOfSize: VERSE_LABEL_FONT_SIZE];
+        verseLabel.textAlignment = UITextAlignmentLeft;
+        verseLabel.textColor = [UIColor darkTextColor];
+        verseLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+        verseLabel.backgroundColor = [UIColor clearColor];
+        [cell.contentView addSubview: verseLabel];
+    }
+    else
+    {
+        webView = (UIWebView*)[cell.contentView viewWithTag: CELLWEBVIEW];
+        verseLabel = (UILabel*)[cell.contentView viewWithTag: CELLLABELVIEW];
+    }
+
+    VerseEntry * entry = [searchData objectAtIndex:[indexPath row]];
+    verseLabel.text = [NSString stringWithFormat:@"%@ %d:%@", entry.book, entry.chapter,entry.verses ];
+    [webView loadHTMLString:[self searchHTMLString:entry.text] baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]]; 
+    return cell;
+}
+/*
 {
     static NSString *CellIdentifier = @"Cell";
     
@@ -66,6 +122,8 @@
     
     return cell;
 }
+*/
+
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	return NO;
